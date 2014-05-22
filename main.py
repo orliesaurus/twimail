db35f2ecb7e026c503312a186f2dd410b820f056
orliesaurus <orliesaurus@facebook.com>
2014-05-22 11:47:42 +0100

    working version is up

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from gmailvariables import *
 
 #login into gmail with a user and a password
 g = gmail.login(mylogin, mypass)
-head = {'content-type': 'application/json'}
-
+#ready the payload
+payload = {'From': '+441277424151', 'To': '+447895445918'}
 #set the filter/flag on all unread emails
 unread = g.inbox().mail(unread=True)
 # Iterate through all unread emails IDs
@@ -14,22 +14,43 @@ unread = g.inbox().mail(unread=True)
 # print body of email
 
 #create a list that will hold all subjects
-subject_array = []
-#iterate through each unread email
-for unreadmail in unread:
-#first fetch the actual messages
-    unreadmail.fetch()
-#check if the msg has been processed already
-    if "twilio-d" in unreadmail.labels:
-        print "skipping"
-    else:
-        #the message hasnt been twilio'd (marked as read)
-        unreadmail.add_label("twilio-d")
-        #only  give us the subject
-        subject_array.append(unreadmail.subject)
-#Next bit ->
-#then we can send it to twilio who will then forward it to my email
-payload = {'From':'+441277424151','To':'+447895445918','Body':'hi'}
-print json.dumps(payload)
-r = requests.post(furl, data=json.dumps(payload))
-print r.text
+def read_email():
+    subject_array = []
+    #iterate through each unread email
+    for unreadmail in unread:
+        #first fetch the actual messages
+        unreadmail.fetch()
+        #check if the msg has been processed already
+        if "twilio-d" in unreadmail.labels:
+            print "skipping"
+        else:
+            #the message hasnt been twilio'd (marked as read)
+            unreadmail.add_label("twilio-d")
+            #only  give us the subject
+            subject_array.append(unreadmail.subject)
+    return subject_array
+
+#Concatanate the email subjects with this "-" char
+def concat_array(array):
+    return "Subjects: " + " | ".join(array)
+    
+
+#uplifting this function to chunk into SMS size the message.
+def chunks(l, n):
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+
+#this function looks for unread emails
+subj = read_email()
+mypayload = concat_array(subj)
+sms_text = chunks(mypayload, 140)
+
+for i in sms_text:
+    payload['Body'] = i
+    #Next bit ->
+    #then we can send it to twilio who will then forward it to my email
+    #so let's prepare a payload and send it to twilio!
+    r = requests.post(furl, data=payload)
+
+#print r.text
